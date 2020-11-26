d75f2bf83fa020841d628a456434bb6cf109aee8
Snobrost <74957909+Snobrost@users.noreply.github.com>
2020-11-26 12:21:30 +0100

    corecttion upadate

--- src/api/controllers/schoolController.js
+++ src/api/controllers/schoolController.js
@@ -71,18 +71,28 @@ exports.get_a_school = (req, res) => {
 }
 
 exports.update_a_school = (req, res) => {
-    School.findByIdAndUpdate(req.params.school_id, req.body, {
-        new: true
-    }, (error, school) => {
-        if (error) {
-            res.status(500);
-            console.log(error);
-            res.json({
-                message: "Erreur serveur."
+    School.find({name: req.body.name ,location : req.body.location}, (error, school) => {
+        if (school.length < 1){
+            School.findByIdAndUpdate(req.params.school_id, req.body, {
+                new: true
+            }, (error, school) => {
+                if (error) {
+                    res.status(500);
+                    console.log(error);
+                    res.json({
+                        message: "Erreur serveur."
+                    })
+                } else {
+                    res.status(200);
+                    res.json(school)
+                }
             })
-        } else {
-            res.status(200);
-            res.json(school)
+        }else {
+            res.status(409);
+                console.log(error);
+                res.json({
+                    message: "Ecole déjà existante."
+                })
         }
     })
 }
@@ -98,7 +108,7 @@ exports.delete_a_school = (req, res) => {
         } else {
             res.status(200);
             res.json({
-                message: "Article supprimé !"
+                message: "Ecole supprimée !"
             })
         }
     })
